a00ed1d440a82f11aeddfafcd5bc6d97dd6e1681
Piotr Zaniewski <piotrzan@gmail.com>
2025-11-24 20:03:37 +0100

    fix: remove lua rocks installation

--- test/minimal_init.lua
+++ test/minimal_init.lua
@@ -1,6 +1,12 @@
 -- Minimal init for running tests
 vim.opt.rtp:prepend('.')
 
+-- Add plenary to rtp if installed in standard location (for CI)
+local plenary_path = vim.fn.expand('~/.local/share/nvim/site/pack/vendor/start/plenary.nvim')
+if vim.fn.isdirectory(plenary_path) == 1 then
+  vim.opt.rtp:prepend(plenary_path)
+end
+
 -- Disable unnecessary plugins
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
